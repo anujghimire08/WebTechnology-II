71d3b783dbf6d84190cf118fc4b590e22a3bbca3
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-28 19:53:03 +0545

    Variables

---
+++ 02-Variables/variables.php
@@ -0,0 +1,22 @@
+<?php 
+// case - sensitive
+$name="xyz";
+$NAME="abc";
+$namE = "wuv";
+
+$a = $b = 10;
+
+$nam = "user";
+$$nam = "Anuj";
+
+echo $a;
+echo "<br>";
+
+echo $name;
+echo "<br>";
+
+echo $nam;
+echo "<br>";
+
+echo $user;
+?>
