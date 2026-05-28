c4969aaa2d6490010deccbfd087b65b7eaae79f9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-28 20:56:04 +0545

    PHP  with HTML

---
+++ 03-PHPwithHTML/index.php
@@ -0,0 +1,19 @@
+<?php
+  $textColor="purple";
+  echo "<h1 style='color:red'>Welcome to PHP</h1>";
+  echo "<i style='color:blue'>Coded wih care @anujghimire</i>"
+?>
+<?php
+  $name="Anuj Ghimire";
+  echo "<h3 style='color:brown'>$name</h3>";
+?>
+
+<h1>
+  It's your boy <?php echo $name?>
+</h1>
+
+<h4 style="color:<?php echo $textColor?>">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, rem?
+</h4>
+
+<h5 style="color:<?php echo $textColor?>">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, aspernatur
+</h5>
