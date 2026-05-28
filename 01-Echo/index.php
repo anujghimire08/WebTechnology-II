f5d111396eb07a8d687177bbdb2c2e3741d30546
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-28 19:22:29 +0545

    echo

---
+++ 01-Echo/index.php
@@ -0,0 +1,20 @@
+<?php 
+echo "hello PHP!<br>";echo "Day-1"; 
+echo "<br>";
+echo ("More to go......");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>
+    <?php echo "HTML output from web server"?> </p>
+</body>
+
+</html>
