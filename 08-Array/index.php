ee33a9e7a55d58ba80c00b779bab425985edabe2
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-29 09:45:10 +0545

    Array

---
+++ 08-Array/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  $arr = ["xyx","abc","def","jkl"];
+  // echo var_dump($arr);
+  // echo $arr[2];
+
+  // for($i = 0;$i < count($arr);$i++) echo "$arr[$i]<br>";  
+  foreach($arr as $val) echo "$val <br>";
+
+?>
