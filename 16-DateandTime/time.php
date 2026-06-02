d5e2b1a3d22ae7bb62c4df08581fb6c999b38c0c
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-02 09:21:01 +0545

    Add PHP script to display formatted date and time

---
+++ 16-DateandTime/time.php
@@ -0,0 +1,14 @@
+<?php
+  date_default_timezone_set("Asia/Kathmandu");
+  // while(1){
+  //   echo date("Y-F-d(D)");
+  //   echo "<br>";
+  //   echo date("h:i:sa");
+  //   sleep(1);
+  // }
+
+    echo date("Y-F-d(D)");
+    echo "<br>";
+    echo date("h:i:sA");
+  
+?>
