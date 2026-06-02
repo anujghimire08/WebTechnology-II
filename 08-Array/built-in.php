ad63f818fbbb8e7a024a60ccc33a76b4b8e3795c
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-02 10:13:21 +0545

    Add PHP array manipulation examples using built-in functions

--- 08-Array/built-in.php
+++ 08-Array/built-in.php
@@ -24,8 +24,37 @@
     print_r($arr);
 
     // splice
-    array_splice($arr ,2);
+    array_splice($arr ,2,1);
     echo "<br>";
     print_r($arr);
+
+    // -------------------------------
+
+    echo "<br>-----------------------------------------<br>";
+
+    $users=["ram","shyam","hari","sita","gita","ram"];
+    // $users="xsdj";
+    echo (is_array($users))? "it's array<br>" : "it's not array<br>";
+
+    unset($users[2]); // specific arr val del
+    print_r($users);
+    echo "<br>";
+
+    echo implode(",",$users); // arr to str
+    echo "<br>";
+
+    $nam = "abc,def,ghi";
+    print_r(explode(",",$nam));
+    echo "<br>";
+
+    
+    $users1=["ram","shyam","hari","sita","gita","rita"];
+    print_r(array_merge($users,$users1));
+    $mergearr = array_merge($users,$users1);
+    echo "<br>";
+
+
+    print_r(array_unique($mergearr));
+
 
 ?>
