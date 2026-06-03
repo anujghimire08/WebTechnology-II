c9f03a28414c7ed6acbeef3702fb9f4eec29a394
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-03 12:22:47 +0545

    basic web creation

---
+++ 18-BasicWebsite/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <nav>
+    <ol>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="about.php">about</a></li>
+      <li><a href="contact.php">contact</a></li>
+    </ol>
+  </nav>
+  <p>copyright &copy; <b>@anujghimire08</b>
+  </p>
+</footer>
---
+++ 18-BasicWebsite/header.php
@@ -0,0 +1,10 @@
+<header>
+  <img src="php.png" alt="php">
+  <nav>
+    <ol>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="about.php">about</a></li>
+      <li><a href="contact.php">contact</a></li>
+    </ol>
+  </nav>
+</header>
