e0038ddf1d82e35a132ba66975c3649f8cb85571
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-03 10:31:29 +0545

    add file upload handling with validation and content reading in PHP

--- 17-FormHandling/form_validation2.php
+++ 17-FormHandling/form_validation2.php
@@ -3,17 +3,14 @@
     // print_r($_SERVER);
     // print_r($_FILES);
     // echo "</pre>";
-
     echo "<h1> Reading From User File..........</h1><br>";
-    if($_SERVER["REQUEST_METHOD"]==="POST" && isset($_FILES["user_file"]["name"])){
+    if($_SERVER["REQUEST_METHOD"]==="POST" && isset($_FILES["user_file"]) && $_FILES["user_file"]["error"]===0){
       // echo "<pre>"; 
       // print_r($_FILES);
       // echo "</pre>";
 
       echo "<br>";
-      echo file_get_contents($_FILES["user_file"]["tmp_name"]) ?? die("not found");
-    }else{
-      echo "File: not found!!";
+      echo file_get_contents($_FILES["user_file"]["tmp_name"]) ?? "File: not found!!";
     }
     
 ?>
