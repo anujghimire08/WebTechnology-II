900ae9aa20170ca7a46fa6a52fbc2ddecb60bed9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-04 13:38:34 +0545

    Add PDO database connection with error handling

---
+++ 19-PHPwithMySQL/Database Connectivity/pdo.php
@@ -0,0 +1,18 @@
+<?php
+
+  $host = "localhost";
+  $username = "root";
+  $password = null;
+  $database = "employee";
+
+  try{
+ 
+  $connection = new PDO("mysql:host=$host;dbname=$database",$username,$password);
+  // var_dump($connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION));
+  $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  echo "connection success";
+  }catch(PDOException $err){
+    die("connection failed:" . $err->getMessage());
+  }
+
+?>
