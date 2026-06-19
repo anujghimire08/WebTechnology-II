4289a2665a87ec9675295eb99b41e4471e1f5541
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 13:50:44 +0545

    Area Calculation

---
+++ PHPPracticeQuestions/04-ArithmeticOperators/qn25.php
@@ -0,0 +1,6 @@
+<?php 
+
+ /*Area Calculation -> Calculate the area of a rectangle (length=15, width=8) and display "The area is [result] square units".  */
+ 
+ $length=15; $width=8;
+ echo "The area is ". $length * $width ." square units";
