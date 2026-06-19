8b6fd0fa6866278852b1d086bf1f56b06899c4e2
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 10:49:55 +0545

    Variable Declaration

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn6.php
@@ -0,0 +1,10 @@
+<?php 
+
+ /* Variable Declaration -> Declare variables for your name, age, and height, then display them
+using echo. */
+
+$username = "xyz";
+$age = 100;
+$height = 5.6;
+
+echo $username . $age . $height;
