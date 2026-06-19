eb16e8ed22fbb73abd1b2f47a6b3253986b596dc
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 22:42:51 +0545

    Function with Parameters

---
+++ PHPPracticeQuestions/11-Functions/qn61.php
@@ -0,0 +1,13 @@
+<?php
+
+ /*Function with Parameters -> Create a function called calculateArea(length,width) that calculates and returns the area of a rectangle. */
+
+
+
+ function calculateArea($length,$width) : int {
+
+ return $length * $width;
+
+ }
+
+ echo calculateArea(10,2);
