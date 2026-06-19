6c92e7c3088582e9fcf91f421e677fc9cd70a606
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 13:57:15 +0545

    Compound Assignment

---
+++ PHPPracticeQuestions/05- AssignmentOperators/qn27.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Compound Assignment -> Start with $x = 10. Use compound assignment operators (+=, -
+=, *=, /=, %=) to modify and display x after each operation.*/
+
+$x = 10;
+
+echo "Initial value: $x <br>";
+
+$x += 5;
+echo "After += 5: $x <br>";
+
+$x -= 3;
+echo "After -= 3: $x <br>";
+
+$x *= 2;
+echo "After *= 2: $x <br>";
+
+$x /= 4;
+echo "After /= 4: $x <br>";
+
+$x %= 3;
+echo "After %= 3: $x <br>";
+
+?>
