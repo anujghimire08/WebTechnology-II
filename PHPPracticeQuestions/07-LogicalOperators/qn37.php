825da5802a3c2a2fc7685f136239c810f27e2b33
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 15:27:34 +0545

    AND Operator

---
+++ PHPPracticeQuestions/07-LogicalOperators/qn37.php
@@ -0,0 +1,7 @@
+<?php 
+
+/*AND Operator -> Check if a number (25) is between 10 and 50 using the AND (&&)
+operator. */
+
+  $num = 25;
+ echo ($num >=10 && $num <=50)? "in between" : "not in between";
