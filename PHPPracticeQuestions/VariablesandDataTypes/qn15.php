f7274c91f8ef105794a621a605cbf90bd0831cb1
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:41:28 +0545

    var_dump Usage

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn15.php
@@ -0,0 +1,17 @@
+<?php
+
+ /*var_dump Usage -> Use var_dump() to display the type and value of the following
+variables: age=25, name = "Ram", price = 99.99, isAcƟve = false, $scores = [85, 90, 78]. */
+
+  $age=25; $name = "Ram"; $price = 99.99; $isActive = false; $scores = [85, 90, 78];
+  var_dump($age);
+  echo "<br>";
+  var_dump($name);
+  echo "<br>";
+  var_dump($price);
+  echo "<br>";
+  var_dump($isActive);
+  echo "<br>";
+  var_dump($scores);
+  echo "<br>";
+  
