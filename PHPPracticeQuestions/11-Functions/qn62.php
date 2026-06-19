9a0c5b45fd1918e3dd274d4fabe7177a41ee32ec
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 22:46:29 +0545

    fun Return Value

---
+++ PHPPracticeQuestions/11-Functions/qn62.php
@@ -0,0 +1,10 @@
+<?php
+
+  /*Return Value -> Write a function called squareNumber($num) that returns the square of the number*/
+
+
+ function squareNumber($num) : int {
+   return $num*$num;
+ }
+
+ echo squareNumber(2);
