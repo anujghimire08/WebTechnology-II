c3ab37a59260147276c4731fd8890a206ac683a0
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 15:24:49 +0545

    Grade Checker

---
+++ PHPPracticeQuestions/06-ComparisionOperator/qn36.php
@@ -0,0 +1,5 @@
+<?php
+
+ /*Grade Checker -> Check if a student's score (75) is greater than or equal to 40 and less than or equal to 100. */
+ $personmark = 75;
+ echo ($personmark >= 40 && $personmark <= 100)? "in between 40 - 100" : "not in between 40 - 100";
