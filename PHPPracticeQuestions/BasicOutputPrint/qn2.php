9b9e66fd09988876f7649e96733d7035d6b6731b
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 10:35:50 +0545

    qn2-Multiple Echo Parameters

---
+++ PHPPracticeQuestions/BasicOutputPrint/qn2.php
@@ -0,0 +1,9 @@
+<?php
+
+  // Multiple Echo Parameters -> Write a PHP script that uses echo with multiple parameters(comma-separated) to display: "PHP" "Programming" "is" "Fun"
+
+  echo "PHP " . "PROGRAMMING " . "is " . "Fun"; 
+
+
+
+
