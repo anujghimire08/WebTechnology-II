f92915d7420fdc05288e5e130c176a77795576e9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 15:11:52 +0545

    Equality vs Identity

---
+++ PHPPracticeQuestions/06-ComparisionOperator/qn32.php
@@ -0,0 +1,11 @@
+<?php
+
+ /*Equality vs Identity -> Compare 5 == "5" and 5 === "5". Display the results and explain the difference. */
+
+ echo '5=="5": ';
+ echo (5=="5")? "true" : "false";
+ echo "<br>";
+ echo '5==="5": ';
+ echo (5==="5")? "true" : "false";
+
+
