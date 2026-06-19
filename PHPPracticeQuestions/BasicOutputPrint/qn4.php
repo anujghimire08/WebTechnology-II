c25bed7a7164f62dd6967b6cd0900d960e223e21
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 10:43:32 +0545

    HTML Integration

---
+++ PHPPracticeQuestions/BasicOutputPrint/qn4.php
@@ -0,0 +1,8 @@
+<?php
+
+  /*  HTML Integration -> Write a PHP script that outputs HTML content with PHP, create a simple webpage with a heading and a paragraph  */
+
+  echo "<h1>PHP with HTML</h1>";
+  echo "<p>Write a PHP script that outputs HTML content with PHP, create a simple webpage with a heading and a paragraph</p>";
+
+
