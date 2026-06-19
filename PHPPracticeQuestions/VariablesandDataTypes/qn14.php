0c541a658de4734e2f2f77243cad21caa1254037
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:37:34 +0545

    Boolean Conditions

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn14.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* Boolean Conditions -> Create a boolean variable $isStudent = true. Write an if-else
+statement that displays "Access Granted" if true, otherwise "Access Denied" */
+
+ $isStudent = true;
+ if($isStudent) echo "Access Granted"; else echo "Access Denied";
+ 
+
