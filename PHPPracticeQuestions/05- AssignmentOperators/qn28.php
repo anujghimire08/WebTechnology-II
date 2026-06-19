50bc5fda11bbe19898ffb0b185516abea40f4a7a
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 14:01:35 +0545

    Increment/Decrement operators

---
+++ PHPPracticeQuestions/05- AssignmentOperators/qn28.php
@@ -0,0 +1,17 @@
+<?php
+
+ /*Increment/Decrement -> Demonstrate pre-increment and post-increment operators.Show how a++ and ++a differ */
+
+
+ #post-increment operators
+ $num = 10;
+ echo'Before $num++: ' . $num . '<br>';
+ $num++;
+ echo'After $num++: ' . $num . '<br>';
+
+ #pre-increment operators
+ echo'Before ++$num: ' . $num . '<br>';
+ ++$num;
+ echo'After ++$num: ' . $num . '<br>';
+
+
