e4657559c8ec1964b61b2cc3aa3a41f0c6013867
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 13:38:34 +0545

    Modulo Operator

---
+++ PHPPracticeQuestions/04-ArithmeticOperators/qn22.php
@@ -0,0 +1,7 @@
+<?php 
+
+ /*Modulo Operator -> Write a script that checks if a number is even or odd using the modulo operator (%). 
+*/
+
+$userinput = rand(1,30);
+echo ($userinput%2===0)? "even number" : "odd number";
