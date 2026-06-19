c6540f5293179b00791599ddfdffa1db5aa12259
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:03:52 +0545

    Case Sensitivity

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn9.php
@@ -0,0 +1,11 @@
+<?php
+
+  /*Case Sensitivity -> Create two variables: name and Name with different values. Display both to demonstrate case sensitivity.*/
+
+
+  $name = "xoxo";
+  $Name = "XOXO";
+
+  echo $name;
+  echo "<br>";
+  echo $Name;
