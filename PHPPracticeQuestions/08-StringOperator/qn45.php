427150129a30e4d5237cc61aacf4b34f71bc3ceb
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 19:52:59 +0545

    Email Generation

---
+++ PHPPracticeQuestions/08-StringOperator/qn45.php
@@ -0,0 +1,10 @@
+<?php
+
+  /*Email Generation -> Generate an email address by concatenating firstName, adot(.),lastName, and "@example.com". */
+
+
+  $firstName = "ramesh";
+  $lastName = "karki";
+
+  echo "Email: " . $firstName . ".". $lastName . "@example.com";
+
