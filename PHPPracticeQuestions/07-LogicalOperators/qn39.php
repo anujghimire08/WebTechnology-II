5fa8622e11792e9a29b0e09f143deb842aaf9474
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 18:52:06 +0545

    NOT (!) operator

---
+++ PHPPracticeQuestions/07-LogicalOperators/qn39.php
@@ -0,0 +1,7 @@
+<?php
+
+ /*. NOT Operator -> Use the NOT (!) operator to check if a variable $isLoggedIn = false. Display "Please Login" if not logged in. */
+
+ $isLoggedIn = false; 
+ if(!$isLoggedIn) echo "Please Login" ;
+
