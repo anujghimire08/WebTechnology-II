35f70f7853dff9dd2a44a2bfe9db2e9c144749c2
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 22:38:38 +0545

    Function Definition

---
+++ PHPPracticeQuestions/11-Functions/qn60.php
@@ -0,0 +1,6 @@
+<?php
+
+/*  Function Definition -> Create a simple function called displayGreeting() that echoes "Welcome to PHP Programming!". */
+
+$displayGreeting = fn() =>  "Welcome to PHP Programming!";
+echo $displayGreeting();
