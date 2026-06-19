4d85266bd2918099ae902d981b7937c6ef270049
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 10:52:01 +0545

    Dynamic Typing

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn7.php
@@ -0,0 +1,8 @@
+<?php
+
+ /* Dynamic Typing -> Create a variable named $data. Assign it an integer value, display it, then reassign it to a string value and display it. */
+
+ $data = 20;
+ echo $data;
+ $data = "anuj";
+ echo $data;
