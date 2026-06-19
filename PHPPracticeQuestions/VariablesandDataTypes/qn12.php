d843a47bc834041bc17e5c77f438869f80b7d38d
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:24:23 +0545

    String Concatenation

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn12.php
@@ -0,0 +1,10 @@
+<?php 
+/* String Concatenation -> Concatenate three string variables: firstName, middleName,
+lastName and display the full name */
+
+ $firstName = "Janga ";
+ $middleName = "Bahadhur ";
+ $lastName = "Rana ";
+
+ $fullName =  $firstName . $middleName . $lastName;
+ echo $fullName;
