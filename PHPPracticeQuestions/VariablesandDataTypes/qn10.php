d0697409cdc21c4c501bc311f2e17e0f3c7956fa
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:10:00 +0545

    Integer Operations

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn10.php
@@ -0,0 +1,12 @@
+<?php
+
+ /* Integer Operations -> Declare variables for marks in three subjects (Nepali, English, Math).Calculate and display the total and average marks.*/
+
+  $nep = 10;
+  $eng = 20;
+  $math = 30;
+  $total = $nep + $eng + $math;
+  $avg = $total / 3;
+  echo "Total: " . $total . "<br>";
+  echo "Average Marks: " . $avg;
+
