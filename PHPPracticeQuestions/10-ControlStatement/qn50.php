282e48fec5f739dd30235060bb6b3fdb4646438f
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 20:39:25 +0545

    if-else Statement

---
+++ PHPPracticeQuestions/10-ControlStatement/qn50.php
@@ -0,0 +1,13 @@
+<?php
+
+ /*if-else Statement -> Create a script that checks if a number is positive, negative, or zero.Display appropriate messages. */
+
+  $num = -1;
+
+  if($num === 0){
+    echo "Zero";
+  }elseif($num > 0){
+    echo "Positive";
+  }else{
+    echo "Negative";
+  }
