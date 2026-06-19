e5f130cba107051ac10080220932ce33951209ff
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:30:59 +0545

    Double vs Single Quotes

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn13.php
@@ -0,0 +1,11 @@
+<?php
+
+ /*Double vs Single Quotes -> Create a variable $college = "Himalayan Darshan". Display it using both double quotes and single quotes to show the difference in parsing */
+
+$college = "Himalayan Darshan";
+
+echo "My college name is $college <br>";
+
+echo 'My college name is $college';
+
+// variable interpolation happens in " "
