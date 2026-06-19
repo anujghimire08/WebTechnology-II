33752da77b7eaec01ff0eee31303c5f761b817c3
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:50:49 +0545

    define() Function

---
+++ PHPPracticeQuestions/03-Constants/qn6.php
@@ -0,0 +1,7 @@
+<?php
+ 
+  /*define() Function -> Define a constant using define() for PI with value 3.14159. Calculate
+the area of a circle with radius 7.  */
+
+  define("PI",3.14159);
+  echo PI;
