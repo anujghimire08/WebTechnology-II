d6e62729d406461251855e1708bbaf43ad1809c3
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 12:39:41 +0545

    Constant Case Sensitivity

---
+++ PHPPracticeQuestions/03-Constants/qn19.php
@@ -0,0 +1,10 @@
+<?php
+
+ /* Constant Case Sensitivity -> Define a constant "GREETING" using define() and try to echo it in lowercase to demonstrate case sensitivity.
+ */
+
+  define("GREETING",10);
+  // echo GREETING;
+  echo greeting;
+  /*Fatal error: Uncaught Error: Undefined constant "greeting" in C:\xampp\htdocs\WebTechnology-II\PHPPracticeQuestions\03-Constants\qn19.php:7 Stack trace: #0 {main} thrown in C:\xampp\htdocs\WebTechnology-II\PHPPracticeQuestions\03-Constants\qn19.php on line 7 */
+
