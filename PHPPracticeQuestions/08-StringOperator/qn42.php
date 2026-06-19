2bd11328b272412331fef363c52238a6d4f9e449
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 18:59:54 +0545

    Concatenation Operator

---
+++ PHPPracticeQuestions/08-StringOperator/qn42.php
@@ -0,0 +1,7 @@
+<?php 
+
+ /*Concatenation Operator -> Combine firstName="Sita" and lastName = "Sharma" using the concatenation operator (.).*/
+
+  $firstName="Sita"; $lastName = "Sharma";
+  echo $firstName . " " . $lastName;
+
