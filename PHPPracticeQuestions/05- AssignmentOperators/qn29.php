2240eb7200d2b10184c702c2037facbfe51a4cd4
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 14:38:46 +0545

    Assignment Chain

---
+++ PHPPracticeQuestions/05- AssignmentOperators/qn29.php
@@ -0,0 +1,7 @@
+<?php 
+
+   /*Assignment Chain -> Chain assignments: $a = $b = $c = 50. Display all three variables to verify they have the same value.*/
+   
+   $a = $b = $c = 50;
+
+  echo $a . $b . $c;  // same
