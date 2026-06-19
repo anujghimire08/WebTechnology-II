c737be358be0f47eff6d3891374c48b7d9db052f
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 15:14:47 +0545

    Age Check

---
+++ PHPPracticeQuestions/06-ComparisionOperator/qn33.php
@@ -0,0 +1,5 @@
+<?php
+
+ /*Age Check -> Check if a person aged 18 is eligible to vote. Display "Eligible" if true,otherwise "Not Eligible". */
+
+ echo (random_int(15,20) > 18)? "Eligible" : "Not Eligible";
