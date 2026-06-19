97f9fb9a9d36dd281cf3df3e882783a3adcb4d2e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 11:00:49 +0545

    Variable Variable (.)

---
+++ PHPPracticeQuestions/VariablesandDataTypes/qn8.php
@@ -0,0 +1,8 @@
+<?php 
+
+ /* Variable Variable ($$) -> Create a variable cityName = "Kathmandu", then use a variable to create a new variable named cityName="Kathmandu", then use a variable variable to create a new variable named Kathmandu with the value "Capital City". Display the result. */
+
+ $data = "name";
+ $$data = "anuj";
+ echo $name;
+ 
