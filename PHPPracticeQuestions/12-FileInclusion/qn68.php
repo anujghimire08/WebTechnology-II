14e05a484843e81ec33bc05c8052e57b26a2cc6d
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-19 23:13:25 +0545

    include_once

---
+++ PHPPracticeQuestions/12-FileInclusion/qn68.php
@@ -0,0 +1,6 @@
+<?php
+
+ /*include_once -> Create a file named "config.php" containing database configuraƟon
+constants. Include it in another script using include_once. */
+
+  include_once("config.ini.php");
