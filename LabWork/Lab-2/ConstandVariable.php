476040f2a754c0d9fe49b807304669211d0aefd2
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-30 09:04:28 +0545

    conditional

--- LabWork/Lab-2/ConstandVariable.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  define("COLLEGE", "Himalaya Darshan College");
-  $college = "XYZ";
-  
-  // COLLEGE is immutable where $college is mutable , value can be change 
-  // constants variable can be define using define() or const keyword 
