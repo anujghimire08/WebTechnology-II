554606f19e5900ef0883304605b644771ff4a9c7
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-06-30 09:06:26 +0545

    PrimitivesandMath

---
+++ LabWork/Lab-2/PrimitivesandMath.php
@@ -0,0 +1,9 @@
+<?php 
+ $productGrocery = 5;
+ $productClothes = 2;
+ $priceperGrocery = 20;
+ $priceperClothes = 20;
+
+ echo "Price: " . $productGrocery * $priceperGrocery;
+echo "<br>";
+ echo "Total Items in cart " . $productClothes + $productGrocery;
