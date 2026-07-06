362c1f09dc0c7037aa285420bc1ba5c5fa049cca
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-07-06 22:26:43 +0545

    Operator Matrix Evaluation

--- LabWork/Lab-2/OperatorMatrixEvaluation.php
+++ LabWork/Lab-2/OperatorMatrixEvaluation.php
@@ -1,34 +1,38 @@
-<?php 
+<?php
+
 $val = "sdjsdk";
 $val1 = 2;
 $val2 = 3;
-$isNumber = is_numeric($val)? true : false;
 
-echo "use of +:" . $val1 + $val2 . "<br/>";
-echo "use of -:" . $val1 - $val2 . "<br/>";
-echo "use of *:" . $val1 * $val2 . "<br/>";
-echo "use of /:" . round( $val1 / $val2) . "<br/>";
-echo "use of %:" . $val1 % $val2 . "<br/>";
+echo is_numeric($val) ? "Numeric<br>" : "Not Numeric<br>";
 
+echo "Use of + : " . ($val1 + $val2) . "<br>";
+echo "Use of - : " . ($val1 - $val2) . "<br>";
+echo "Use of * : " . ($val1 * $val2) . "<br>";
+echo "Use of / : " . round($val1 / $val2) . "<br>";
+echo "Use of % : " . ($val1 % $val2) . "<br>";
 
-$val1+=$val2;
-echo "use of +=:" . $val1 . "<br/>";
+$val1 += $val2;
+echo "Use of += : " . $val1 . "<br>";
 
+$val1 -= $val2;
+echo "Use of -= : " . $val1 . "<br>";
 
-$val1-=$val2;
-echo "use of -=:" . $val1 . "<br/>";
+if($val1 % 2 == 0 && $val2 % 2 == 0)
+    echo "Both numbers are even.<br>";
 
-if($val1 % 2 === 0 && $val2 % 2 === 0 ) echo "even number<br>";
-if(!$val1 % 2 === 0 && !$val2 % 2 === 0 ) echo "odd number<br>";
-if($val1 % 2 === 0 || $val2 % 2 === 0 ) echo "both number<br>";
+if($val1 % 2 != 0 && $val2 % 2 != 0)
+    echo "Both numbers are odd.<br>";
 
+if($val1 % 2 == 0 || $val2 % 2 == 0)
+    echo "At least one number is even.<br>";
 
-echo "before:" . $val1 . "<br>";
+echo "Before Increment: $val1<br>";
 $val1++;
-echo 'after $val++: ' . $val1 . "<br>";
+echo "After Post Increment (val1++): $val1<br>";
 
-echo "before:" . $val1 . "<br>";
+echo "Before Decrement: $val1<br>";
 --$val1;
-echo 'after --$val: ' . $val1;
+echo "After Pre Decrement (--val1): $val1<br>";
 
-
+?>
