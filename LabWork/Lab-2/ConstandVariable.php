158e9ea45d825e3a934333cc347e5a1d6e82735a
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-07-06 23:04:59 +0545

    ConstandVariable

--- LabWork/Lab-2/ConstandVariable.php
+++ LabWork/Lab-2/ConstandVariable.php
@@ -1,6 +1,10 @@
 <?php 
   define("COLLEGE", "Himalaya Darshan College");
   $college = "XYZ";
+
+  echo "below txt is Constant";
+  echo "<br>";
+  echo COLLEGE;
   
   // COLLEGE is immutable where $college is mutable , value can be change 
   // constants variable can be define using define() or const keyword 
