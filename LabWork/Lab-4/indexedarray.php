2513ef804abd78fca352efdc733276aa863cce72
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-07-16 03:16:46 +0545

    Create indexedarray.php

---
+++ LabWork/Lab-4/indexedarray.php
@@ -0,0 +1,18 @@
+<?php
+
+$months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+for ($i = 0; $i < count($months); $i++) echo $months[$i] . "<br>";
