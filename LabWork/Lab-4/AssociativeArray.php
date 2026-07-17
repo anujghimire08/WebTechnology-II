86c013363721e10053248556ef7bc1c3523f1bd1
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-07-17 06:27:12 +0545

    Create AssociativeArray.php

---
+++ LabWork/Lab-4/AssociativeArray.php
@@ -0,0 +1,17 @@
+<?php
+
+    $months = [
+    "jan" => "31 Days",
+    "feb" => "29 Days",
+    "mar" => "30 Days",
+    "apr" => "29 Days",
+    "may" => "31 Days",
+    "jun" => "31 Days",
+    "jul" => "29 Days",
+    "aug" => "31 Days",
+    "sept" => "30 Days",
+    "oct" => "31 Days",
+    "nov" => "30 Days",
+    ];
+
+    foreach($months as $key=>$val) echo " " . $key . " has " . $val . "<br>";
