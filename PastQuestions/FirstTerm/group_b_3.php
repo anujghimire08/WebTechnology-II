0e6c3675b2d5785b56ee324a0452e7a5291c593b
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-08-01 10:43:44 +0545

    group_b_3

---
+++ PastQuestions/FirstTerm/group_b_3.php
@@ -0,0 +1,15 @@
+<?php
+    switch($val=2){
+      case 1: 
+      echo "SEM: 1";
+      break;
+      case 2: 
+      echo "SEM: 2";
+      break;
+      case 3: 
+      echo "SEM: 3";
+      break;
+      case 4: 
+      echo "SEM: 4";
+      break;
+    }
