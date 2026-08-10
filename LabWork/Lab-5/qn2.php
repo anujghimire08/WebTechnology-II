80e160ed1e065da2af0ad0fd69eeffc91a906ea0
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-08-10 07:53:07 +0545

    qn2

--- LabWork/Lab-5/qn2.php
+++ LabWork/Lab-5/qn2.php
@@ -1,9 +1,11 @@
 <?php 
  class Pet {
-  function __construct(private string $name , private int $price){}
+  function __construct(public string $name , public int $price){}
   function __destruct()
   {
     echo('Object Removed');
   }
  }
  $dog = new Pet("Votey", 12000);
+ echo nl2br("Dog Name: " . $dog->name  ."\nDog Price:" . $dog->price, true);
+echo "<br/>";
