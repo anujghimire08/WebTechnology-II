114223d7a91023b9d3d88c62de95027f3a293bf9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-08-10 08:40:01 +0545

    qn5

--- LabWork/Lab-5/qn5.php
+++ LabWork/Lab-5/qn5.php
@@ -2,12 +2,15 @@
 
 function divide($a, $b): int
 {
+  if ($b == 0) {
+      throw new Exception("can't / by 0");
+  }
   return $a / $b;
 }
 
  try{
   divide(2, 0);
  }
- catch(Error){
-  echo "can't / by 0";
+ catch(Exception $e){
+  echo $e;
  }
